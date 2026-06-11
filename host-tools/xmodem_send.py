73c6695c676044415b1daa9c7ec9d41cedcc536b
Harshdeep <hs4907@nyu.edu>
2026-06-10 23:57:13 -0400

    Phase 2: Working XMODEM channel -- Can send one packet succesfully

--- host-tools/xmodem_send.py
+++ host-tools/xmodem_send.py
@@ -9,7 +9,10 @@ NAK = 0x15
 
 def calculate_checksum(data):
     # sum all bytes and return lowest 8 bits
-    pass
+    sum = 0
+    for byte in data:
+        sum += byte
+    return sum & 0xFF
 
 def send_file(port, filename):
     # open serial port
@@ -17,4 +20,66 @@ def send_file(port, filename):
     # wait for initial NAK from STM32
     # send packets one by one
     # send EOT when done
-    pass
+
+    # So far I am only sending packet
+
+    ser = serial.Serial(port=port,baudrate=115200,timeout=None)
+    time.sleep(0.5)
+    ser.reset_input_buffer()
+    ser.reset_output_buffer()
+
+    print("Listening for Start")
+    # Waiting for initial NAK from STM32
+    listening = True
+    while (listening):
+        if ser.in_waiting > 0:
+            data = ser.read(1)
+            if data and data[0] == NAK:
+                print("Starting Communication")
+                listening = False
+
+    packet_num = 1
+    packet = bytearray([SOH, packet_num, (~packet_num) & 0xFF])
+    checksum = 0
+
+    with open(filename, "rb") as f:
+        data = f.read(16)
+        data = data.ljust(128, b'\x1A')  # 0x1A is standard XMODEM padding
+        # byte_data = bytes([int(data,2)])
+        packet.extend(data)
+        checksum = calculate_checksum(data)
+        packet.append(checksum)
+
+    f.close()
+
+    print(f"Sending Packet...{time.time()}")
+    ser.write(packet)
+    print("Succesfully Sent Packet")
+
+    listening = True
+    while (listening):
+        if ser.in_waiting > 0:
+            data = ser.read(1)
+            if data and data[0] == ACK:
+                print(f"STM32 Successfully Recieved Packet: {packet_num}")
+                listening = False
+                packet_num += 1
+
+    ser.write(bytes([EOT]))
+
+    listening = True
+    while (listening):
+        if ser.in_waiting > 0:
+            data = ser.read(1)
+            if data and data[0] == ACK:
+                print("Successfully Ended Communication")
+                listening = False
+                packet_num += 1
+
+    ser.close()
+
+
+def main():
+    send_file("COM6","app.bin")
+
+main()
